106605eea6e7b3023fc1f98d092b1cdda87ee563
Aaron Iker <aaron@anoma.ly>
2026-02-01 08:19:28 +0100

    version script

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,4 @@
 declare const __PACKAGE_VERSION__: string | undefined;
 
 export const VERSION =
-  typeof __PACKAGE_VERSION__ === 'string' && __PACKAGE_VERSION__.length > 0
-    ? __PACKAGE_VERSION__
-    : '0.0.0';
+  typeof __PACKAGE_VERSION__ === 'string' && __PACKAGE_VERSION__.length > 0 ? __PACKAGE_VERSION__ : '0.0.0';
